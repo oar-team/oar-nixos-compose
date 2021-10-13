361530966cf2f62f4bbca6bddad1d299a10245f8
Olivier Richard <olivier.richard@imag.fr>
2021-10-13 08:37:43 +0200

    basic: update nix directory

--- basic/nix/all-in-one.nix
+++ basic/nix/all-in-one.nix
@@ -1,17 +1,5 @@
-{ pkgs, flavour, allConfig, buildOneconfig }:
+{ pkgs, flavour, compositionName, allConfig, buildOneconfig }:
 let
-
-  baseConfig = buildOneconfig "" { };
-  baseImage =
-    pkgs.runCommand "image" { buildInputs = [ pkgs.nukeReferences ]; } ''
-      mkdir $out
-      cp ${baseConfig.config.system.build.kernel}/bzImage $out/kernel
-      echo "init=${
-        builtins.unsafeDiscardStringContext
-        baseConfig.config.system.build.toplevel
-      }/init ${toString baseConfig.config.boot.kernelParams}" > $out/cmdline
-      nuke-refs $out/kernel
-    '';
 
   machinesInfo =
     pkgs.lib.mapAttrs (n: m: m.config.system.build.initClosureInfo) allConfig;
@@ -21,20 +9,19 @@ let
     pkgs.lib.mapAttrsToList (n: m: "${m.closure_info}") machinesInfo;
   allStorePaths = map (x: "${x}/store-paths") allClosureInfo;
 
-  allSquashfsStore = pkgs.stdenv.mkDerivation {
-    name = "all-squashfs.img";
-
-    nativeBuildInputs = [ pkgs.squashfsTools ];
+  allStoreInfo = pkgs.stdenv.mkDerivation {
+    name = "all-store-info";
 
     buildCommand = ''
+      mkdir $out
       sort ${
         builtins.concatStringsSep " " allStorePaths
-      } | uniq > merged-store-paths
+      } | uniq > $out/merged-store-paths
 
-      IFS=', ' read -r -a allClosureInfo <<< "${
+      IFS=' ' read -r -a allClosureInfo <<< "${
         builtins.concatStringsSep " " allClosureInfo
       }"
-      IFS=', ' read -r -a allRoles <<< "${
+      IFS=' ' read -r -a allRoles <<< "${
         builtins.concatStringsSep " " allRoles
       }"
       allRegistrations=""
@@ -42,34 +29,20 @@ let
       for index in "''${!allClosureInfo[@]}"
       do
         source="''${allClosureInfo[$index]}"/registration
-        target=nix-path-registration"''${allRoles[$index]}"
-        cp $source $target
+        target=nix-path-registration-${compositionName}-"''${allRoles[$index]}"
+        cp $source $out/$target
         echo $source $target
         allRegistrations="$allRegistrations $target"
       done
 
-      # Generate the squashfs image.
-      mksquashfs $allRegistrations $(cat merged-store-paths) $out \
-        -keep-as-directory -all-root -b 1048576 -comp gzip -Xcompression-level 1;
+      echo $allRegistrations > $out/all-registration
+
     '';
-  };
-
-  allRamdisk = pkgs.makeInitrd {
-    inherit (baseConfig.config.boot.initrd) compressor;
-    prepend = [ "${baseConfig.config.system.build.initialRamdisk}/initrd" ];
-
-    contents = [{
-      object = allSquashfsStore;
-      symlink = "/nix-store.squashfs";
-    }];
   };
 
 in {
   nodes = machinesInfo;
-  all = {
-    squashfs_img = "${allSquashfsStore}";
-    initrd = "${allRamdisk}/initrd";
-    kernel = "${baseImage}/kernel";
-    qemu_script = "${baseConfig.config.system.build.qemu_script}";
-  };
+  all_store_info = "${allStoreInfo}";
+  #nodesInit = pkgs.lib.mapAttrs (n: m: "${m.config.system.build.toplevel}/init")
+  #  allConfig;
 }
