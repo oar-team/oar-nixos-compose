614d0bc7456715a94036d791378c25d9a205ba36
Olivier Richard <olivier.richard@imag.fr>
2025-08-25 17:16:38 +0200

    [redox] swithc nxc-25.05

--- redox/flake.nix
+++ redox/flake.nix
@@ -1,32 +1,23 @@
 {
   description = "OAR - from master";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    nxc.url = "gitlab:nixos-compose/nixos-compose/24.11?host=gitlab.inria.fr";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nxc.url = "gitlab:nixos-compose/nixos-compose/25.05?host=gitlab.inria.fr";
     nxc.inputs.nixpkgs.follows = "nixpkgs";
     NUR.url = "github:nix-community/NUR";
     kapack.url = "github:oar-team/nur-kapack/oar-redox";
     kapack.inputs.nixpkgs.follows = "nixpkgs";
-    nixpkgs-master.url = "github:NixOS/nixpkgs";
   };
 
-  outputs = { self, nixpkgs, nxc, NUR, kapack, nixpkgs-master}:
+  outputs = { self, nixpkgs, nxc, NUR, kapack}:
     let
       system = "x86_64-linux";
-      
     in {
       packages.${system} = nxc.lib.compose {
         inherit nixpkgs system NUR;
         repoOverrides = { inherit kapack; };
         composition = ./composition.nix;
         setup = ./setup.toml;
-        
-        overlays = [
-           (self: super: {
-              hwloc = nixpkgs-master.legacyPackages.${system}.hwloc;
-           })
-        ];
-        
       };
       
       devShell.${system} = nxc.devShells.${system}.nxcShell;
